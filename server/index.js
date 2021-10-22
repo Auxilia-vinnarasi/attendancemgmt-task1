47b399d58b60442c1fca56e95e814c77a73ddc0b
Auxilia Vinnarasi <auxiliavinnarasi@gmail.com>
2021-10-22 18:16:40 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use("/attendance", attendanceRoutes);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(path.join(__dirname, "frontend/build"));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend","build","index.html"))
