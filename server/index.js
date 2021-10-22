6e183771a1703f9d00dee5d1b8941e0f664d5639
Auxilia Vinnarasi <auxiliavinnarasi@gmail.com>
2021-10-22 14:13:14 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -23,17 +23,15 @@ app.use("/attendance", attendanceRoutes);
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/build")));
+if (process.env.NODE_ENV === "production") 
+{
+  app.use("/",express.static("frontend/build"));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend","build","index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
+  app.get("*", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "frontend/build/index.html"))
   });
-}
+} 
+
 app.use(errorHandler);
 app.use(notFound);
 
